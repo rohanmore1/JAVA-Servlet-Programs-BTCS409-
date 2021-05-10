7daa797f5cc24cebf1d13903cfc29d7f23242e62
rohanmore1 <69668795+rohanmore1@users.noreply.github.com>
2021-05-10 15:32:31 +0530

    Add files via upload

---
+++ Servlet1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Hp
+ */
+@WebServlet(urlPatterns = {"/Servlet1"})
+public class Servlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        try
+        {
+         
+        out.println("<h2>Welcome to my first servlet application in neatbeans</h2>");
+        } finally{
+            out.close();
+        }
+    
+
+    }
+}
